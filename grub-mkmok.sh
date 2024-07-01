6506f5f14c8de10a4550ef64f90201ad3bb64118
rysndavjd <rysndavjd@protonmail.com>
2024-07-01 12:43:20 +0200

    made grub-mkmok.sh make more sense

--- grub-mkmok.sh
+++ grub-mkmok.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]
 then 
@@ -6,16 +6,20 @@ then
     exit 1
 fi
 
-openssl req -newkey rsa:2048 -nodes -keyout MOK.key -new -x509 -sha256 -subj "/CN=MOK key/" -out MOK.crt
-openssl x509 -outform DER -in MOK.crt -out MOK.cer
+if ! command openssl 2>/dev/null; then
+    echo "Openssl not found. Is it installed?"  
+fi
 
-mkdir -p /root/mok
-mv -n ./MOK.crt ./MOK.key ./MOK.cer /root/mok
-if [[ $? == 1 ]]
+if [ -e "/root/mok/MOK.key" ]
 then 
-    echo "mok keys already exist not overwriting"
-    rm ./MOK.crt ./MOK.key ./MOK.cer
+    echo -e "MOK keys already exist in /root/mok\nNot overwriting."
+    exit 2
 else
+    mkdir -p /root/mok
+    cd /root/mok
+    openssl req -newkey rsa:2048 -nodes -keyout MOK.key -new -x509 -sha256 -subj "/CN=MOK key/" -out MOK.crt
+    openssl x509 -outform DER -in MOK.crt -out MOK.cer
     chmod 700 /root/mok -R
-    echo "Mok keys created and moved to /root/mok/"
-fi 
+    echo -e "MOK keys created in /root/mok"
+    exit 0
+fi
